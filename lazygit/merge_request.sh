02912222bd8a63e785084dc4963480217609c80b
Hadi Shahpuri <hadishahpuri@gmail.com>
2024-07-20 16:13:58 +0330

    🐛 [-] Gitlab merge request bug fixed

--- lazygit/merge_request.sh
+++ lazygit/merge_request.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "************************"
+echo "******** Target branch *********"
 echo $1
 
-echo "--------------------------"
-echo $2
+localBranch=$(git rev-parse --abbrev-ref HEAD)
+echo "******** Local branch *********"
+echo $localBranch
 
-git remote get-url origin | grep gitlab && git push -u origin HEAD -o merge_request.create -o merge_request.target=$1 -o merge_request.title=$2 || git push -u origin HEAD
+git remote get-url origin | grep gitlab && git push -u origin HEAD -o merge_request.create -o merge_request.target=$1 -o merge_request.title=$localBranch || git push -u origin HEAD
