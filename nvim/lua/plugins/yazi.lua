0eeb6bca5721ef99e1eb1e8737e1a3a3c687f568
Hadi Shahpuri <hadishahpuri@gmail.com>
2024-09-09 16:09:32 +0330

    ✨ [-] Some bugs fixed

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -5,15 +5,9 @@ return {
     keys = {
         -- 👇 in this section, choose your own keymappings!
         {
-            "<leader>-",
+            "<leader>e",
             "<cmd>Yazi<cr>",
             desc = "Open yazi at the current file",
-        },
-        {
-            -- Open in the current working directory
-            "<leader>e",
-            "<cmd>Yazi cwd<cr>",
-            desc = "Open the file manager in nvim's working directory",
         },
         {
             -- NOTE: this requires a version of yazi that includes
