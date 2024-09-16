1ec091fb9af056a1b2d316c17b0e0a5ec3fb6137
Hadi Shahpuri <hadishahpuri@gmail.com>
2024-09-16 14:01:38 +0330

    ✨ [-] Eww bar style updated

--- bing_wallpaper.sh
+++ bing_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rand=$(shuf -i1-9 -n1)
 fileName=~/Pictures/bing_wallpaper.jpg
-url=$(proxychains curl 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&mkt=en-US' | jq -r ".images[0].url")
+url=$(curl 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&mkt=en-US' | jq -r ".images[0].url")
 url="https://bing.com$url"
 echo $url
-$(proxychains wget -O $fileName $url)
+$(aria2c -d ~/Pictures -o bing_wallpaper.jpg --continue=true $url)
