a55b053eebf35181d2e624c74f7bb708ecbd9f28
Hadi Shahpuri <hadishahpuri@gmail.com>
2024-10-08 15:54:29 +0330

    🐛 [-] A bug fixed

--- nvim/lua/plugins/theme-rose-pine.lua
+++ nvim/lua/plugins/theme-rose-pine.lua
@@ -75,5 +75,8 @@ return {
         -- vim.cmd("colorscheme rose-pine-main")
         -- vim.cmd("colorscheme rose-pine-moon")
         -- vim.cmd("colorscheme rose-pine-dawn")
+        vim.cmd [[hi Folded guibg=NONE]]
+        -- vim.cmd [[hi Folded guifg=#737aa2]]
+        vim.cmd [[hi Folded ctermbg=NONE]]
     end
 }
